448acb1a236ddf593b31b036392a1d4d1675ff45
Muhamad Nopriyansyah <43802252+iyanlowe@users.noreply.github.com>
2018-10-16 22:37:03 -0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,5 +1,12 @@
 Public Class Animal {
+ int jml_kaki;
+ String warna;
+ String j_makan;
 
-
-
+  void static Bernafas(){
+    System.out.printl(" Hewan Bernafas ... " );
+  }
+  void static Bergerak (){
+    System.out.printl(" Hewan Bergerak ... ");
+  }
 }
